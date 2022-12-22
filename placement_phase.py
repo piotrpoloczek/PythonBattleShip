1a1ecb58fd4114e897c012200585494a5ec992f7
Niram11 <dominikwidera2@gmail.com>
2022-12-22 11:26:33 +0100

    dodanie funkji place ship,niedokoczona

--- placement_phase.py
+++ placement_phase.py
@@ -46,22 +46,6 @@ def translate_coordinates(coordinates):
     return (coordinate_x,coordinate_y-1)
 
 
-def ask_for_coordinates(board):
-    while True:
-        coordinates = input('enter coordinates')
-        coordinate_x, coordinate_y= translate_coordinates(coordinates)
-        if check_coordinate_is_in_board(coordinate_x,coordinate_y) and check_coordinate_is_free(board,coordinate_x,coordinate_y):
-            return (coordinate_x,coordinate_y)
-        else:
-            print('Bad coordinates')
-    
-
-
-def ship_placement(board):
-    pass
-
-
-
 def check_available_coordinates(board):
     available_coordinates = []
     for row_index, row in enumerate(board):
@@ -70,13 +54,66 @@ def check_available_coordinates(board):
                 available_coordinates.append((row_index, item_index))
     return available_coordinates
 
+def ask_for_coordinates(board):
+    while True:
+        coordinates = input('enter coordinates\n')
+        coordinate_x, coordinate_y= translate_coordinates(coordinates)
+        if check_coordinate_is_in_board(coordinate_x,coordinate_y) and check_coordinate_is_free(board,coordinate_x,coordinate_y):
+            return (coordinate_x,coordinate_y)
+        else:
+            print('Bad coordinates')
+
+def number_of_ships():
+    number_of_ships = [1,1,1,1,2,2]
+    return number_of_ships
+
+def ask_for_orientation():
+    while True:
+        orientation = input('You want to place the ship in horizontal or vertical?\n')
+        orientation = orientation.lower()
+        if orientation == 'horizontal' or orientation == 'vertical':
+            return orientation
+        else:
+            print('Bad orientation')
+
+
+def place_ship(board,coordinate_x,coordinate_y,orientation,ship_size):
+    board_copy = board
+    if orientation == 'vertical':
+        for i in range(ship_size):
+            if True:
+                board_copy[coordinate_x][coordinate_y] = 'X'
+                coordinate_y += 1
+        print(board)
+        return board
+    elif orientation == 'horizontal':
+        pass
+
+def ships_placement(board):
+    print(board)
+    ships_to_place = number_of_ships()
+    for i in ships_to_place:
+        print(f'place ship of size {i}')
+        coordinate_x,coordinate_y = ask_for_coordinates(board)
+        orientation = ask_for_orientation()
+        board = place_ship(board,coordinate_x,coordinate_y,orientation,i)
+        print(board)
+
+
 def ask_user_input():
     pass
 
 def validate_user_input():
     pass
 
-print(ask_for_coordinates(board=[['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0']]))
+
+
+board=[['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0'],['0', '0', '0', '0','0']]
+place_ship(board,coordinate_x=0,coordinate_y=0,orientation='vertical',ship_size=4)
+
+
+
+
 
 
 
