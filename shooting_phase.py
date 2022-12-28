e00c030e9ccb3842330cc22d0a08352e0342355b
Lukasz Nowak <lukasz83nowak@gmail.com>
2022-12-28 19:04:56 +0100

    clear screen procedure

--- shooting_phase.py
+++ shooting_phase.py
@@ -1,3 +1,4 @@
+from os import system, name
 
 # swiching player function
 def change_player(player):
@@ -17,30 +18,45 @@ def row_number(board_size = 5):
         rows.append(chr(i))
     return rows
 
+def quit_from_application(quit_command):
+     if quit_command.upper() == 'QUIT':
+            exit()
 
-
+def clear_screen():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 #function validate size of input string, scope of input string and return coordinates in form (x,y) and current player after move.
 def user_input(size_of_the_board,current_player):
-    while True:
-        
-        user_shoot = input("Make a shoot :").upper() 
     
+    while True:
+        user_shoot = input("Make a shoot :").upper()
+
+        quit_from_application(user_shoot)
+   
+#validation of input
+       
         if len(user_shoot) == 2 and user_shoot[1].isalpha() != True \
             and user_shoot[0].isalpha() == True \
             and int(user_shoot[1]) in range(1, size_of_the_board + 1) \
             and int(user_shoot[0] in row_number(size_of_the_board)):
-          break
+            break
         else:
+            clear_screen()
             continue
-    
-    row = row_number(size_of_the_board)
-    res_user_shoot = (row.index(user_shoot[0]),int(user_shoot[1])-1)
+
+
+    res_user_shoot = (row_number(size_of_the_board).index(user_shoot[0]),int(user_shoot[1])-1)
     current_player = change_player(current_player)
+   
     return res_user_shoot, current_player
+
+
   
 def main():      
-  
+   
     coortinates, current_player = user_input(6, current_player = 'Player_a')
     print(current_player, coortinates)
    
