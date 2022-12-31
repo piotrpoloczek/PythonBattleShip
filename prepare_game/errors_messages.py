19f38e7fe3d69b2deeec7f671db30314b3383d9d
piotrpoloczek <piotr.jan.poloczek@gmail.com>
2022-12-31 06:59:06 +0100

    fix issues with functions in module prepare_game

--- prepare_game/errors_messages.py
+++ prepare_game/errors_messages.py
@@ -5,6 +5,5 @@ def board_size_error():
     print(f'You provided incorrect input, please choose the size from \
 {MIN_BOARD_SIZE} to {MAX_BOARD_SIZE} at most.')
 
-
 def invallid_username_error():
     print('Please choose the corect name for the user.')
