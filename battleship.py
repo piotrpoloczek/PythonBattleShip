12fa42ee269e6ec982af6b4cdef0d30c235c3f69
Niram11 <dominikwidera2@gmail.com>
2023-01-07 11:54:21 +0100

    zaimportowanie funkcji ships placement

--- battleship.py
+++ battleship.py
@@ -1,13 +1,13 @@
 from prepare_game.prepare_game import prepare_game
 from printing_board.printing import print_board
-from placement_phase import placement
+from placement_phase.placement_phase import ships_placement
 #from shooting_phase import shooting
 
 
 def main_loop():
     players = prepare_game()
     print(players)
-    players = placement(players)
+    players = ships_placement(players)
     #shooting(players)
 
     #example how to use the print_board function in other modules
